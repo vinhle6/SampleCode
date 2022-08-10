ee5b3e5a2f613aa840abdf8a803550660fc37ec5
Le Hoang Vinh <vinh.le@terralogic.com>
2022-08-10 10:57:59 +0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Vinh Le on 8/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SampleCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCodeApp.swift
+//  Shared
+//
+//  Created by Vinh Le on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
